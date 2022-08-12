16326642ffb6177f00898cb6ba102bf1076d73a0
Matthew Pocock <turingatemyhamster@gmail.com>
2022-08-12 15:35:30 +0100

    Added support for pyindustry. Gated roads logic behind mods checks so that the mods are now optional. Also made them optional.

--- src/data.lua
+++ src/data.lua
@@ -1,21 +1,40 @@
 -- inspired by drone-roads
+local arci_prefix = "Arci-"
+
+if mods["AsphaltPaving"] then
+    local transport_system_technology = "transport-system"
+    local aslphalt_technology = "Arci-asphalt"
 
 
+    -- gate transport drones behind asphalt
+    if data.raw.technology[transport_system_technology] then
+        table.insert(data.raw.technology[aslphalt_technology].prerequisites, transport_system_technology)
+    end
 
--- gate transport drones behind asphalt
-local transport_system_technology = "transport-system"
-local aslphalt_technology = "Arci-asphalt"
-if data.raw.technology[transport_system_technology] then
-    table.insert(data.raw.technology[aslphalt_technology].prerequisites, transport_system_technology)
+    -- set the acri road tiles to being drone road tiles
+    for name, value in pairs(data.raw.item) do
+        if name:find(arci_prefix, 1, true) == 1 then
+            value.is_road_tile = true
+        end
+    end
+
+    -- for space exploration enable the asphalt tiles in space
+    if mods["space-exploration"] then
+        -- set the acri road tiles to being drone road tiles
+        for name, value in pairs(data.raw.item) do
+            if name:find(arci_prefix, 1, true) == 1 then
+                table.insert(value.place_as_tile.condition, spaceship_collision_layer)
+            end
+        end
+    end
 end
 
-
-
--- set the acri road tiles to being drone road tiles
-local arci_prefix = "Arci-"
-for name, value in pairs(data.raw.item) do
-    print(name)
-    if name:find(arci_prefix, 1, true) == 1 then
-        value.is_road_tile = true
+-- for py
+if mods["pyindustry"] then
+    -- set py-aslphalt road tiles to being drone road tiles
+    for name, value in pairs(data.raw.item) do
+        if name:find("py-asphalt", 1, true) == 1 then
+            value.is_road_tile = true
+        end
     end
 end
